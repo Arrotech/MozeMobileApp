9937fbe66d8630995eaee7feded53f16ebfc874d
Arrotech <arrotechdesign@gmail.com>
2019-10-19 11:56:54 +0300

    Feature (Location) View Services by Location
    What does this pr do?
    
    Have a user able to view services by location and occupation.
    
    Any related pivotal stories?
    
    [Finishes #169243832]

--- app/src/main/java/com/example/moze/Services/model/ServiceList.java
+++ app/src/main/java/com/example/moze/Services/model/ServiceList.java
@@ -1,4 +1,34 @@
 package com.example.moze.Services.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceList {
+
+    private String message;
+
+    @SerializedName("service")
+    private List<Service> servicesLocation;
+
+    private String status;
+
+    public ServiceList(String message, List<Service> servicesLocation, String status) {
+        this.message = message;
+        this.servicesLocation = servicesLocation;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<Service> getServicesLocation() {
+        return servicesLocation;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
